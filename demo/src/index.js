ac2c519daecc3ebfe5128fe33a81dceae6dbf8b6
wpb119 <wpb119@gmail.com>
2023-08-24 14:27:11 -0500

    add new adapter & change index.js

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,14 @@
 
 import mirador from 'mirador/dist/es/src/index';
 import annotationPlugins from '../../src';
-import LocalStorageAdapter from '../../src/LocalStorageAdapter';
-import AnnototAdapter from '../../src/AnnototAdapter';
+import AnnotationStoreAdapter from '../../src/AnnotationStoreAdapter';
 
-const endpointUrl = 'http://127.0.0.1:3000/annotations';
+const SERVER_URL = 'http://127.0.0.1:8000';
+
 const config = {
   annotation: {
-    adapter: (canvasId) => new LocalStorageAdapter(`localStorage://?canvasId=${canvasId}`),
-    // adapter: (canvasId) => new AnnototAdapter(canvasId, endpointUrl),
-    exportLocalStorageAnnotations: false, // display annotation JSON export button
+    adapter: (canvasId) => new AnnotationStoreAdapter(canvasId, SERVER_URL),
+    orageAnnotations: false, // display annotation JSON export button
   },
   id: 'demo',
   window: {
